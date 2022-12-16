78d0b0ccd0c23f426b7a33014673f2098923088a
Coco-pseudo <82671782+Coco-pseudo@users.noreply.github.com>
2022-12-16 16:07:51 +0100

    Update projet.c

--- projet.c
+++ projet.c
@@ -3,17 +3,25 @@
 #include <math.h>
 #include <projet.h>
 
-// to do list: fonction absolu, finir creation fils, finir creation arbre, finir le projet :)
+// to do list: creer fonction equilibrage d'avl, finir creation fils, finir creation arbre, finir le projet :)
+// verifier si lors de l'equilibre, il faut que avoir acces au parent du noeud pour certaines rotations
+// verifier les fonctions de calcul d'équilibre
 typedef int type;
 typedef struct arbre {
     struct arbre * fd;
     struct arbre * fg;
     type elmt;
     int equilibre;
+    int doublon;
 } Arbre ;
 typedef Arbre * pA;
 
-int max(int a, int b){
+
+
+type absolu (type a){
+    return (a>=0 ? a : -a);
+}
+int max(type a, type b){
     return (a<b ? a : b);
 }
 int estVide(pA a){
@@ -25,6 +33,35 @@ int descendance(pA a){
         if(!estVide(a->fg) || !estVide(a->fd))return 1;
     }
     return 0;
+}
+pA rechercheParentCreation(pA a, type e){ //dans le cas ou un element est egal a celui que l'on cree, on renvoie sa localisation, sinon c le futur parent.
+    if(!estVide(a)){
+        if(e< a->elmt){
+            if(!estVide(a->fg)){
+                if(e == a->fg->elmt){
+                    return a->fg;
+                }else{
+                    return rechercheParentCreation(a->fg, e);
+                }
+            }else{
+                return a;
+            }
+        }else{
+            if(e> a->elmt){
+                if(!estVide(a->fd)){
+                    if(e == a->fd->elmt){
+                        return a->fd;
+                    }else{
+                        return rechercheParent(a->fd, e);
+                    }
+                }else{
+                    return a;
+                }
+            }else{//a-> elmt == e (a est la racine de l'arbre d'origine)
+                return a;
+            }
+        }
+    }
 }
 
 pA rechercheParent(pA a, type e){
@@ -59,30 +96,55 @@ pA rechercheParent(pA a, type e){
 
 pA creationFils (pA papa, pA enfant, int info){ //info sert a savoir si on le trie est AVL ou ABR
     if(papa->elmt < enfant->elmt){
-        enfant->fg = papa->fg; //cas où pour une raison qlq papa a un fg
         papa->fg = enfant;
         if(info == 1){
             papa ->equilibre = papa->equilibre - 1;
-            equilibrage(papa);
-            if(!estVide(enfant->fg))enfant->equilibre = -absolu(enfant->equi)
+            verifEquilibre(papa);
         }
-    }else
-    if(papa->elmt == enfant->elmt)exit(2); //données egales, avoir si on doit exit ou creer un branchement dans l'arbre pour tous ses enfants dont l'element est egal au sien
-        enfant->fd = papa->fd;
+    }else{
+        if(papa->elmt == enfant->elmt)exit(2); 
         papa->fd = enfant;
-        papa ->equilibre = papa->equilibre + 1;
+        if(info == 1){
+            papa ->equilibre = papa->equilibre + 1;
+            verifEquilibre(papa);
+        }
+    }
 }
 
-int verifEquilibre(pA a){ //retourne si |l'equilibre| d'un des noeuds est  >1
+/*int verifEquilibre(pA a){ //retourne si |l'equilibre| d'un des noeuds est  >1
     if(!estVide(a)){
         return (verifEquilibre(a->fg)<verifEquilibre(a->fd) ? verifEquilibre(a->fg) : verifEquilibre(a->fd));
     }
+}*/
+int profondeurDescendance (pA a){
+    if(!estVide(a)){
+        if(!descendance(a)){
+            return max(profondeurDescendance(fg)+1, max(profondeurDescendance(fd)+1));
+        }
+        return 1;
+    }return 0;
 }
-void creationArbre(type elmt){
+void calculEquilibre (pA a){
+    if(!estVide(a)) a->equilibre = profondeurDescendance(fg) - profondeurDescendance(fd);
+}
+int verifEquilibre (pA a){ //si |equilibre de l'arbre >= 2, modifie 
+    if(!estVide(a)){
+        if(absolu(a->equilibre >=2)){
+            //faire appel aux fonctions d'equilibrage
+
+        }
+    }else return 1;
+}
+void creationArbre(pA arbre, type elmt){
+    pA tmp =rechercheParentCreation(a,elmt);
+    if (tmp->elmt = elmt){
+        tmp->doublon = tmp->doublon + 1
+        return;
+    }
     pA new= malloc(sizeof(Arbre));
-    pA tmp =rechercheParent(elmt);
     new -> elmt = elmt;
     new->equilibre = 0;
+    new->doublon = 0;
     new = creationFils(tmp,new);
 }
 
