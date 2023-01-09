c5fd2a9f26bee342eb0b51598d9c891c06468105
Coco-pseudo <82671782+Coco-pseudo@users.noreply.github.com>
2023-01-09 18:00:57 +0100

    Update projet.c

--- projet.c
+++ projet.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#include <projet.h>
+//#include <projet.h>
 
 // to do list: creer fonction equilibrage d'avl, finir creation fils, finir creation arbre, finir le projet :)
 // verifier si lors de l'equilibre, il faut que avoir acces au parent du noeud pour certaines rotations
@@ -9,6 +9,8 @@
 //commencer a chercher comment extraire les données du fichier pour l'incrémenter et faire le tri
 //chercher comment creer un fichier de sorti
 //mettre a jour le projet.h
+//continuer fonctions d'equilibrage et faire les fcts de pivot
+//rajouter la partie recherche des elements dans le fichier en parametre et creer un fichier qui contiendras les donnees tries
 typedef int type;
 typedef struct arbre {
     struct arbre * fd;
@@ -18,8 +20,8 @@ typedef struct arbre {
     int doublon;
 } Arbre ;
 typedef Arbre * pA;
-
-
+//a supprimer la liste suivante de prototypes
+int verifEquilibre (pA);
 
 type absolu (type a){
     return (a>=0 ? a : -a);
@@ -55,7 +57,7 @@ pA rechercheParentCreation(pA a, type e){ //dans le cas ou un element est egal a
                     if(e == a->fd->elmt){
                         return a->fd;
                     }else{
-                        return rechercheParent(a->fd, e);
+                        return rechercheParentCreation(a->fd, e);
                     }
                 }else{
                     return a;
@@ -122,13 +124,13 @@ pA creationFils (pA papa, pA enfant, int info){ //info sert a savoir si on le tr
 int profondeurDescendance (pA a){
     if(!estVide(a)){
         if(!descendance(a)){
-            return max(profondeurDescendance(fg)+1, max(profondeurDescendance(fd)+1));
+            return max(profondeurDescendance(a->fg)+1,profondeurDescendance(a->fd)+1);
         }
         return 1;
     }return 0;
 }
 void calculEquilibre (pA a){
-    if(!estVide(a)) a->equilibre = profondeurDescendance(fg) - profondeurDescendance(fd);
+    if(!estVide(a)) a->equilibre = profondeurDescendance(a->fg) - profondeurDescendance(a->fd);
 }
 int verifEquilibre (pA a){ //si |equilibre de l'arbre >= 2, modifie 
     if(!estVide(a)){
@@ -138,30 +140,61 @@ int verifEquilibre (pA a){ //si |equilibre de l'arbre >= 2, modifie
         }
     }else return 1;
 }
-void creationArbre(pA arbre, type elmt){
+pA rechercheParentModifEquilibre(pA a, type e){ //dans le cas ou un element est egal a celui que l'on cree, on renvoie sa localisation, sinon c le futur parent.
+    if(!estVide(a)){
+        if(e< a->elmt){
+            if(!estVide(a->fg)){
+                //modifier l'equilibre
+                a->equilibre = a->equilibre -1;
+                verifEquilibre(a);
+                return rechercheParentModifEquilibre;
+            }else{
+                return a;
+            }
+        }else{
+            if(e> a->elmt){
+                if(!estVide(a->fd)){
+                //modifier l'equilibre
+                a->equilibre = a->equilibre +1;
+                verifEquilibre(a);
+                    return rechercheParentModifEquilibre(a->fd, e);
+                }else{
+                    return a;
+                }
+            }
+        }
+    }
+}
+void creationArbre(pA a, type elmt, int info){
     pA tmp =rechercheParentCreation(a,elmt);
     if (tmp->elmt = elmt){
-        tmp->doublon = tmp->doublon + 1
+        tmp->doublon = tmp->doublon + 1;
         return;
+    }
+    if (info == 1){ //info = 1 veut dire qu'on utilise un avl
+        rechercheParentModifEquilibre(a,elmt);
     }
     pA new= malloc(sizeof(Arbre));
     new -> elmt = elmt;
     new->equilibre = 0;
     new->doublon = 0;
-    new = creationFils(tmp,new);
+    new = creationFils(tmp,new,info);
 }
 
 
 
-
-int main (int a) {// a indique le mode souhaité entre AVL, ABR et tableau
-    printf("a=%d\n",a);
+//info = 1 veut dire qu'on utilise un avl
+int main (int argc, char *argv[]) {// a indique le mode souhaité entre AVL, ABR et tableau
+    char a = argv[1][0];
+    char test = argv [1][1];
+    if (test != '\0') exit (6); //code erreur a determiner
+    printf("a=%c\n",a);
     switch (a){
-        case (1) :printf("1\n") ; //traitement en AVL
+        case ('1') :printf("1\n") ; //traitement en AVL
         break;
-        case 2 :printf("2\n") ; //traitement en ABR 
+        case ('2') :printf("2\n") ; //traitement en ABR 
         break;
-        case 3 :printf("3\n")  ;//traitement en tableau
+        case ('3') :printf("3\n")  ;//traitement en tableau
         break;
         default : 
             printf("erreur dans l'option\n"); //ne devrait pas arriver car testé dans le script
